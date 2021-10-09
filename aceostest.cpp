e74fd5ca36f4998693a455ab722afd1c10b0a7eb
dale0110 <4265024+dale0110@users.noreply.github.com>
2021-10-09 12:21:38 +0800

    Update aceostest.cpp

--- aceostest.cpp
+++ aceostest.cpp
@@ -10,9 +10,12 @@ g++     aceostest.cpp    -o  aceostest.x -I/home/kevin/ACE_wrappers    -lACE -pt
 
 
 
-cl aceostest.cpp /EHsc /DWIN32 /W4 /D_DEBUG /MDd -ID:/tim/ume-tim-cpp/code/server/pal/server/Tim400/TimSubItems_Z102/src -ID:/compile_tools_ume/devtools/snacc_vc7/c++-lib/inc -ID:/compile_tools_ume/devtools/snacc_vc7/c++-lib/inc/additional -ID:/compile_tools_ume/devtools/snacc_vc7/include/c++ -ID:/compile_tools_ume/devtools/snacc_vc7/include/c++/additional -I. -I../ -I../include -I../export -ID:/compile_tools_ume/devtools/ace56_vc7 -ID:/compile_tools_ume/devtools/ace56_vc7/ACEXML/common -ID:/compile_tools_ume/devtools/ace56_vc7/ACEXML/parser/parser -ID:/compile_tools_ume/devtools/ace56_vc7/TAO -ID:/compile_tools_ume/devtools/ace56_vc7/TAO/tao -ID:/compile_tools_ume/devtools/ace56_vc7/TAO/tao/portableserver -ID:/compile_tools_ume/devtools/ace56_vc7/TAO/tao/strategies -ID:/compile_tools_ume/devtools/ace56_vc7/TAO/tao/valuetype -ID:/compile_tools_ume/devtools/ace56_vc7/TAO/tao/messaging -ID:/compile_tools_ume/devtools/ace56_vc7/TAO/orbsvcs -ID:/compile_tools_ume/devtools/ace56_vc7/TAO/orbsvcs/orbsvcs -ID:/compile_tools_ume/devtools/xerces/include -ID:/tim/ume-tim-cpp/code/server -ID:/tim/ume-tim-cpp/code/server/output/include -ID:/tim/ume-tim-cpp/code/server/interface/psl -ID:/tim/ume-tim-cpp/code/server/interface/pfl -ID:/tim/ume-tim-cpp/code/server/interface/pfl/cmddispatcher -ID:/tim/ume-tim-cpp/code/server/interface/pfl/dbutils -ID:/tim/ume-tim-cpp/code/server/interface/pfl/pflutils -ID:/tim/ume-tim-cpp/code/server/interface/pfl/southadapter -ID:/tim/ume-tim-cpp/code/server/interface/pfl/southdistributor -ID:/tim/ume-tim-cpp/code/server/interface/pfl/forword -ID:/tim/ume-tim-cpp/code/server/interface/pfl/timerservice -ID:/tim/ume-tim-cpp/code/server/interface/pal/palutils -ID:/tim/ume-tim-cpp/code/server/psl/context/corba/idl/lib -ID:/tim/ume-tim-cpp/code/server/psl/pslutils/export -ID:/tim/ume-tim-cpp/code/server/psl -ID:/tim/ume-tim-cpp/code/server/psl/kernel/export -ID:/tim/ume-tim-cpp/code/server/pfl/dbutils/dboperate/sybase/lib/sybase/OCS-15_0/include -ID:/tim/ume-tim-cpp/code/server/../public/qx-interface/objPublic -ID:/tim/ume-tim-cpp/code/server/../public/qx-interface/objCombine -ID:/tim/ume-tim-cpp/code/server/pfl/southdistributor/export -ID:/tim/ume-tim-cpp/code/server/interface/pal/server/resource -ID:/tim/ume-tim-cpp/code/server/interface/pal/server/tpmanager -ID:/tim/ume-tim-cpp/code/server/pal/server/Tim400/TimServer/src -ID:/tim/ume-tim-cpp/code/server/pal/server/Tim400/TimSubItems_COMM/src -ID:/compile_tools_ume/devtools/boost_1_34_1 /link D:/compile_tools_ume/devtools/ace56_vc7/lib/ACE.lib  /OUT:aceostest.x
+set path="C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/Hostx86/x86";C:/Windows/System32
 
-
+set include="C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/um";"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt"
+set lib="C:/Program Files (x86)/Windows Kits/10/Lib/10.0.19041.0/ucrt/x86";"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.19041.0/um/x86"
+cl aceostest.cpp /EHsc /DWIN32 /W4 /D_DEBUG /MDd -IC:/Users/kevin/Desktop/ACE_wrappers -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/um" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/ucrt" -I"C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0/shared" -I"C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/include" /link C:/Users/kevin/Desktop/ACE_wrappers/lib/ACEd.lib "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/lib/x86/msvcprtd.lib" "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/lib/x86/msvcrtd.lib" "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/lib/x86/oldnames.lib" "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/lib/x86/vcruntimed.lib" /OUT:aceostest.x
+cmd
 
 
 
@@ -40,7 +43,7 @@ using namespace std;
 
 
 
-int main()
+int main(int argc, char** argv)
 
 {
 
@@ -54,11 +57,11 @@ int main()
 
 
 
-    while(1)
+    while (1)
 
     {
 
-        if((ACE_OS::gettimeofday() - tv).sec() == 5)
+        if ((ACE_OS::gettimeofday() - tv).sec() == 5)
 
         {
 
